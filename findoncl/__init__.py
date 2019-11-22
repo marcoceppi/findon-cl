e9da379bbe27119abb30e58402aed1af0133fd72
Marco Ceppi <marco@ceppi.net>
2019-11-22 05:51:07 -0500

    :wrench: Updating setup.py and setup.cfg with actual contents

---
+++ findoncl/__init__.py
@@ -0,0 +1,8 @@
+
+__title__ = 'findon-cl'
+__version__ = '0.0.1'
+__description__ = 'Find things on CL!'
+__author__ = 'Marco Ceppi'
+__author_email__ = 'marco@ceppi.net'
+__url__ = 'https://github.com/marcoceppi/findon-cl'
+__license__ = 'GNU General Public License v3.0'
---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import os
+import re
+
+from pathlib import Path
+from typing import List
+
+from codecs import open
+from setuptools import find_packages, setup
+
+
+here = Path(__file__).parent
+
+
+# Load the package's __init__.py file as a dictionary.
+pkg = {}
+with open(here / 'findoncl' / '__init__.py', 'r', 'utf-8') as f:
+    pkg = {k: v for k, v in re.findall(r"^(__\w+__) = \'(.+)\'", f.read(), re.M)}
+
+# Load the README
+readme = ''
+if os.path.exists(here / 'README.md'):
+    with open(here / 'README.md', 'r', 'utf-8') as f:
+        readme = f.read()
+
+setup(
+    name=pkg['__title__'],
+    version=pkg['__version__'],
+    description=pkg['__description__'],
+    license=pkg['__license__'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url=pkg['__url__'],
+    author=pkg['__author__'],
+    author_email=pkg['__author_email__'],
+    packages=find_packages(),
+    package_data={'': ['LICENSE']},
+    python_requires='>=3.6',
+    install_requires=[
+
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    zip_safe=False,
+)
